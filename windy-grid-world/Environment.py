1df1ae57f783d660f063c1130f78e1f220c944f2
Huy Nguyen <huynguyen@Huys-MacBook-Air.local>
2021-05-25 10:41:13 -0700

    Add Windy Grid-world problem

--- windy-grid-world/Environment.py
+++ windy-grid-world/Environment.py
@@ -1,9 +1,19 @@
 import numpy as np
 
+''' DIRECTIONS '''
+ARROWS = ['←','→','↑','↓']
+ACTIONS = np.array([
+    [0,-1], # LEFT
+    [0,1],  # RIGHT
+    [-1,0], # UP
+    [1,0]   # DOWN
+])
+
+
 # Generate grid world similar to example 6.5 in the book.
 def generate_grid_6_5():
     grid_shape = (7,10)
-    grid = np.zeros(shape=grid_shape)
+    grid = np.zeros(shape=grid_shape, dtype=np.int64)
     # Level 1 wind
     grid[:,3:6] = 1
     grid[:,8] = 1
@@ -11,7 +21,10 @@ def generate_grid_6_5():
     grid[:,6:8] = 2
 
     # Start and goal positions
-    start = (3,0)
-    goal = (3,7)
+    start = [3,0]
+    goal = [3,7]
 
-    return grid, grid_shape, start, goal
+    default_reward = -1.0
+    terminal_reward = 0.0
+
+    return grid, grid_shape, start, goal, default_reward, terminal_reward
---
+++ windy-grid-world/README.md
@@ -0,0 +1,8 @@
+# WINDY-GRID-WORLD
+Using SARSA to solve the Control Problem:
+- Estimate optimal Q.
+- Estimate optimal policies based on estimated Q.
+
+
+## Examples
+![linr](optimal_policies.png)
